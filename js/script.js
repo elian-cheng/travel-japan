233d661b8d3304013450580d5e8f5c2a76e7caa7
Olga Chernega <chernega.o@gmail.com>
2022-08-24 23:34:53 +0300

    feat: add smooth scroll js and simple login form to sign in button

--- js/script.js
+++ js/script.js
@@ -12,9 +12,9 @@
 // Burger handler
 
 (function () {
+  const menuCloseItem = document.querySelector(".header-nav-close");
   const burgerItem = document.querySelector(".burger");
   const menu = document.querySelector(".header-nav");
-  const menuCloseItem = document.querySelector(".header-nav-close");
   burgerItem.addEventListener("click", () => {
     menu.classList.add("header-nav-active");
   });
@@ -22,6 +22,63 @@
     menu.classList.remove("header-nav-active");
   });
 })();
+
+// Scroll to anchors, smooth menu links scrolling
+(function () {
+  const smoothScroll = function (targetEl, duration) {
+    const headerElHeight = document.querySelector(".header").clientHeight; //If the header is not fixed, delete this variable here
+    let target = document.querySelector(targetEl);
+    let targetPosition = target.getBoundingClientRect().top - headerElHeight; //Delete it from here too, just headerElHeight
+    let startPosition = window.pageYOffset;
+    let startTime = null;
+
+    const ease = function (t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    };
+
+    const animation = function (currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, targetPosition, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    };
+    requestAnimationFrame(animation);
+  };
+
+  const scrollTo = function () {
+    const links = document.querySelectorAll(".js-scroll");
+    links.forEach(each => {
+      each.addEventListener("click", function () {
+        const currentTarget = this.getAttribute("href");
+        smoothScroll(currentTarget, 1000);
+      });
+    });
+  };
+  scrollTo();
+})();
+
+// Modal login form
+const modal = document.querySelector("#modal");
+const openModalButton = document.querySelector("#open-modal-btn");
+const closeModalButton = document.querySelector("#close-modal-btn");
+const overlay = document.querySelector("#overlay");
+
+openModalButton.addEventListener("click", () => {
+  modal.classList.add("open");
+  overlay.classList.add("open");
+});
+
+function closeModal() {
+  modal.classList.remove("open");
+  overlay.classList.remove("open");
+}
+
+closeModalButton.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
 // Self-calling function
 // (function () {
