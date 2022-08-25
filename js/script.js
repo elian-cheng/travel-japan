3cda2562c1648ec185f66a91f6ef20341ced22a6
Olga Chernega <chernega.o@gmail.com>
2022-08-26 01:11:51 +0300

    fix: optimize page loading speed and images, fix minor html validation errrors

--- js/script.js
+++ js/script.js
@@ -15,12 +15,25 @@
   const menuCloseItem = document.querySelector(".header-nav-close");
   const burgerItem = document.querySelector(".burger");
   const menu = document.querySelector(".header-nav");
+  const menuLinks = document.querySelectorAll(".header-link");
   burgerItem.addEventListener("click", () => {
     menu.classList.add("header-nav-active");
   });
   menuCloseItem.addEventListener("click", () => {
     menu.classList.remove("header-nav-active");
   });
+  if (window.innerWidth < 769) {
+    // for (let i = 0; i < menuLinks.length; i++) {
+    //   menuLinks[i].addEventListener("click", () => {
+    //     menu.classList.remove("header-nav-active");
+    //   });
+    // }
+    menuLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        menu.classList.remove("header-nav-active");
+      });
+    });
+  }
 })();
 
 // Scroll to anchors, smooth menu links scrolling
